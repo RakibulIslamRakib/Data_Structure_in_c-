1f496d3e214205c338bd5240857164e6a180b957
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2020-06-26 21:26:17 -0700

    Create exceptionhandl1.cpp
    hackerrank 30 days challangr problem

---
+++ exceptionhandl1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string s;
+  cin>>s;
+  try{
+    cout<<stoi(s)<<endl;
+  }
+  catch(exception e){
+      cout<<"Bad String"<<endl;
+  }
+}
